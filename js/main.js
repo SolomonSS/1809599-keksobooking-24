cde6859889f98b8fa6c83abda3526c772a4d2682
Siamen <seemonf@gmail.com>
2021-10-06 20:54:45 +0300

    module4-task1

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
-//Task 2 Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно
-// eslint-disable-next-line no-undef,id-length
+// eslint-disable-next-line no-undef
 const _ = require('lodash');
 const getRandomFloat = (min, max, precision) => {
   if (min < 0) {
@@ -8,49 +7,54 @@ const getRandomFloat = (min, max, precision) => {
   return (Math.random() * (max - min) + min).toFixed(precision);
 };
 
-//Task 1 Функция, возвращающая случайное целое число из переданного диапазона включительно
+
 const getRandomInt = (min, max) => getRandomFloat(min, max, 0);
 
 
-const types = ['palace', 'flat', 'house', 'bungalow'];
-const checkins = ['12:00', '13:00', '14:00'];
-const featuresList = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
-const photosList = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
+const TYPES = ['palace', 'flat', 'house', 'bungalow'];
+const CHECKINS = ['12:00', '13:00', '14:00'];
+const FEATURES = ['wifi', 'dishwasher', 'parking', 'washer', 'elevator', 'conditioner'];
+const PHOTOS = ['https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/duonguyen-8LrGtIxxa4w.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/brandon-hoogenboom-SNxQGWxZQi0.jpg',
   'https://assets.htmlacademy.ru/content/intensive/javascript-1/keksobooking/claire-rendall-b6kAwr1i0Iw.jpg'];
-const getPhotosList = () => {
+const getPhotos = () => {
   const randomSize = Number(getRandomInt(0, photosList.length));
   const result = [];
-  // eslint-disable-next-line id-length
   for (let i = 0; i < randomSize; i++) {
-    result.push(photosList[i]);
+    result.push(PHOTOS[i]);
   }
   return result;
 };
-const getFeatureItem = () => _.sampleSize(featuresList, Number(getRandomInt(0, featuresList.length - 1)));
+const getFeatureItem = () => _.sampleSize(FEATURES, Number(getRandomInt(0, FEATURES.length - 1)));
+const getRandomElement = (list) => list[Number(getRandomInt(0, list.length - 1))];
+const LATMIN = 35.65000;
+const LATMAX = 35.70000;
+const LNGMIN = 139.70000;
+const LNGMAX = 139.80000;
+
 const createElement = () => {
-  const locLat = getRandomFloat(35.65000, 35.70000, 5);
-  const locLng = getRandomFloat(139.70000, 139.80000, 5);
+  const LAT = getRandomFloat(LATMIN, LATMAX, 5);
+  const LNG = getRandomFloat(LNGMIN, LNGMAX, 5);
   return {
     author: {
-      avatar: `${'img/avatars/user0'}${  getRandomInt(1, 10)  }.png`,
+      avatar: `${'img/avatars/user0'}${getRandomInt(1, 10)}.png`,
     },
     offer: {
       title: getRandomInt(1, 5),
-      address: `${locLat} ${locLng}`,
+      address: `${LAT} ${LNG}`,
       price: getRandomInt(500, 1000),
-      type: types[Number(getRandomInt(0, types.length - 1))],
+      type: TYPES[getRandomElement(TYPES)],
       rooms: getRandomInt(1, 6),
       guests: getRandomInt(1, 12),
-      checkin: checkins[Number(getRandomInt(0, checkins.length - 1))],
-      checkout: checkins[Number(getRandomInt(0, checkins.length - 1))],
-      features: Array.from({length: Number(getRandomInt(0, featuresList.length - 1))}, getFeatureItem),
+      checkin: getRandomElement(CHECKINS),
+      checkout: getRandomElement(CHECKINS),
+      features: Array.from({length: getRandomElement(FEATURES)}, getFeatureItem),
       description: 'Some description',
-      photos: getPhotosList(),
+      photos: getPhotos(),
     },
     location: {
-      lat: locLat,
-      lng: locLng,
+      lat: LAT,
+      lng: LNG,
     },
   };
 };
