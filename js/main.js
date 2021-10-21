61da18c94c606b35c1a32d9282fb7beb8b427596
Siamen <seemonf@gmail.com>
2021-10-21 18:18:10 +0300

    module7-task1

--- js/main.js
+++ js/main.js
@@ -2,7 +2,4 @@ import {createElement} from './mocks.js';
 import {createPopup} from './popup.js';
 
 const similarElements = Array.from({length: 10}, createElement);
-//createPopup(similarElements[0]);
-for (let i =0;i<similarElements.length; i++){
-  createPopup(similarElements[i]);
-}
+similarElements.forEach((element) => createPopup(element));
