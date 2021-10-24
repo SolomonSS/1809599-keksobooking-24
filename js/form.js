70ae3ed4bb824c09d39ee879b8358f5b34f156a5
Siamen <seemonf@gmail.com>
2021-10-24 18:08:00 +0300

    module7-task2

--- js/form.js
+++ js/form.js
@@ -4,7 +4,8 @@ const roomsInput = document.querySelector('#room_number');
 const capacity = document.querySelector('#capacity');
 const timeIn = document.querySelector('#timein');
 const timeOut = document.querySelector('#timeout');
-
+const adForm = document.querySelector('.ad-form');
+const mapFilters = document.querySelector('.map__filters');
 
 const TypePrice = {
   BUNGALOW: 0,
@@ -38,3 +39,26 @@ timeIn.addEventListener('change', () => {
   timeOut.value = timeIn.value;
   timeIn.reportValidity();
 });
+
+const unActive = () => {
+  adForm.classList.add('ad-form--disabled');
+  adForm.setAttribute('disabled','disabled');
+  mapFilters.classList.add('ad-form--disabled');
+  mapFilters.setAttribute('disabled','disabled');
+};
+
+const active = () =>{
+  adForm.classList.remove('ad-form--disabled');
+  adForm.removeAttribute('disabled');
+  mapFilters.classList.remove('ad-form--disabled');
+  mapFilters.removeAttribute('disabled');
+};
+unActive();
+active();
+
+
+/*
+    На месте карты отображается серый прямоугольник.
+    Форма заполнения информации об объявлении .ad-form содержит класс ad-form--disabled;
+    Все интерактивные элементы формы .ad-form должны быть заблокированы с помощью атрибута disabled, добавленного на них или на их родительские блоки fieldset;
+    Форма с фильтрами .map__filters заблокирована так же, как и форма .ad-form — на форму добавлен специальный класс, а на её интерактивные элементы атрибуты disabled. */
