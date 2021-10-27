cdf825ac6f6ba8013c643bf3369c12fb0ecbc341
Siamen <seemonf@gmail.com>
2021-10-27 10:39:17 +0300

    module8-task2

--- js/map.js
+++ js/map.js
@@ -1,7 +1,25 @@
 import {setFormEnabled} from './utils.js';
+import * as L from 'leaflet';
 
 const mapFilterForm = document.querySelector('.map__filters');
 
+const map = L.map('map__canvas')
+  .on('load', () => {
+  })
+  .setView({
+    lat: 35.39101,
+    lng: 139.50221,
+  }, 10);
+
+L.tileLayer(
+  'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',
+  {
+    attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+  },
+).addTo(map);
+
+
 const setMapFormEnabled = (enabled) => setFormEnabled(mapFilterForm, enabled, 'ad-form--disabled');
+
 export {setMapFormEnabled};
 
